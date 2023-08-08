90be439a96cde676e4f5686221af56f2aa5564ab
Matheus El Madi <matheuselmadi@gmail.com>
2023-08-08 17:41:50 -0300

    Pagina Sustentabilidade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatNativeDateModule} from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
 
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 
@@ -61,7 +62,8 @@ import { SustentabilidadeComponent } from './sustentabilidade/sustentabilidade.c
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    SlickCarouselModule
+    SlickCarouselModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
