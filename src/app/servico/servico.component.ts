88a27c835e57f381c7b82ec5fdaac2063943f468
Matheus El Madi <matheuselmadi@gmail.com>
2023-10-11 17:22:31 -0300

    Fotos no card do serviço de solda.

--- src/app/servico/servico.component.ts
+++ src/app/servico/servico.component.ts
@@ -7,12 +7,47 @@ import { Component } from '@angular/core';
 })
 export class ServicoComponent {
 
+  imgClick = false;
+  imgUrl = '';
+
+  photos = [
+    {
+      id: 1,
+      imageUrl: 'assets/images/servicos/IMG_1.JPG',
+      caption: 'Entregas',
+      info: 'Informações sobre a Foto 1',
+      showInfo: false
+    },
+    {
+      id: 2,
+      imageUrl: 'assets/images/servicos/IMG_2.JPG',
+      caption: 'Drenagem',
+      info: 'Informações sobre a Foto 2',
+      showInfo: false
+    },
+    {
+      id: 3,
+      imageUrl: 'assets/images/servicos/IMG_3.JPG',
+      caption: 'Quadra de Areia',
+      info: 'Soldagem concluída.',
+      showInfo: true
+    },
+
+  ];
+
   ngOnInit(): void {
     this.scrollToTop();
   }
 
   scrollToTop() {
     window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }
+
+  navigateToDetails(photo: any) {
+    if (photo.id) {
+      this.imgUrl = photo.imageUrl;
+      this.imgClick = true;
+    }
   }
 
 }
