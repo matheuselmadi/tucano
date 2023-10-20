dd92efc76aef335a17cfe51644bd877965dfa74e
Matheus El Madi <matheuselmadi@gmail.com>
2023-10-20 17:29:06 -0300

    Teste Deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { ReactiveFormsModule } from '@angular/forms';
 
